db267e68864c8a448b5073fdd2968d35d2dcfc10
Felipe Antunes <felipeantunes542@gmail.com>
2022-11-20 21:54:35 -0300

    começando projeto producer

---
+++ producer/src/main/java/br/com/fiap/agrodrones/producer/ProducerApplication.java
@@ -0,0 +1,79 @@
+package br.com.fiap.agrodrones.producer;
+
+import br.com.fiap.agrodrones.producer.dto.DroneInfoDTO;
+import br.com.fiap.agrodrones.producer.service.DroneInfoService;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.core.MediaType;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "br.com.fiap.agrodrones.producer")
+public class ProducerApplication {
+
+	@Value("${server.port}")
+	private String serverPort;
+
+	@Value("${fiap.api.path}")
+	private String contextPath;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean servletRegistrationBean() {
+		ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(), contextPath + "/*");
+		servlet.setName("CamelServlet");
+		return servlet;
+	}
+
+
+	@Component
+	class RestApi extends RouteBuilder {
+
+		@Override
+		public void configure() throws Exception {
+			CamelContext context = new DefaultCamelContext();
+
+			rest("/api/")
+					.id("api-route")
+					.post("/drone-info")
+					.produces(MediaType.APPLICATION_JSON)
+					.consumes(MediaType.APPLICATION_JSON)
+					.bindingMode(RestBindingMode.auto)
+					.type(DroneInfoDTO.class)
+					.enableCORS(true)
+					.to("direct:remoteService");
+
+			from("direct:remoteService")
+					.id("direct-route")
+					.tracing()
+					.log(">>> id_drone: ${body.id_drone}")
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							DroneInfoDTO dto = (DroneInfoDTO) exchange.getIn().getBody();
+							DroneInfoService.processDroneInfo(dto);
+							exchange.getIn().setBody(dto);
+						}
+					})
+					.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
+
+
+		}
+	}
+
+}
---
+++ producer/src/main/java/br/com/fiap/agrodrones/producer/config/Config.java
@@ -0,0 +1,23 @@
+package br.com.fiap.agrodrones.producer.config;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+
+public class Config {
+    private static CachingConnectionFactory connectionFactory;
+
+    public static CachingConnectionFactory getConnection(){
+
+        if(connectionFactory == null){
+            connectionFactory = new CachingConnectionFactory("***");//TODO add hostname
+            connectionFactory.setUsername("***");//TODO add username
+            connectionFactory.setPassword("***");//TODO add password
+            connectionFactory.setVirtualHost("***");//TODO add virtualhost
+
+            //Recommended settings
+            connectionFactory.setRequestedHeartBeat(30);
+            connectionFactory.setConnectionTimeout(30000);
+        }
+
+        return connectionFactory;
+    }
+}
