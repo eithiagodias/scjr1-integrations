badb4e851be562f50a621b7b15fcc0a4bf955db6
Raquel Rocha <raquelfr.econ@gmail.com>
2022-12-03 22:39:28 -0300

    apagano credenciais

--- consumer/src/main/java/br/com/fiap/agrodrones/consumer/config/Config.java
+++ consumer/src/main/java/br/com/fiap/agrodrones/consumer/config/Config.java
@@ -8,10 +8,10 @@ public class Config {
     public static CachingConnectionFactory getConnection(){
 
         if(connectionFactory == null){
-            connectionFactory = new CachingConnectionFactory("jackal.rmq.cloudamqp.com");//TODO add hostname
-            connectionFactory.setUsername("ydplgzhw");//TODO add username
-            connectionFactory.setPassword("ZM2BGRHcsmNXLi3aaeqeCxwQyzP47Yhs");//TODO add password
-            connectionFactory.setVirtualHost("ydplgzhw:ydplgzhw");//TODO add virtualhost
+            connectionFactory = new CachingConnectionFactory("xxx");//TODO add hostname
+            connectionFactory.setUsername("xxx");//TODO add username
+            connectionFactory.setPassword("xxx");//TODO add password
+            connectionFactory.setVirtualHost("xxxx");//TODO add virtualhost
 
             //Recommended settings
             connectionFactory.setRequestedHeartBeat(30);
